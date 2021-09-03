02c64d60b90fcf5b13643a0523bf4c5c1bef7c8d
anfelo <af.osorio1341@gmail.com>
2021-09-03 10:39:45 -0500

    Added user login and register

--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/lib/pq v1.10.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 )
--- web/handler.go
+++ web/handler.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"context"
 	"html/template"
 	"net/http"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/anfelo/goreddit"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/google/uuid"
 	"github.com/gorilla/csrf"
 )
 
@@ -25,10 +27,12 @@ func NewHandler(
 	threads := ThreadHandler{store: store, sessions: sessions}
 	posts := PostHandler{store: store, sessions: sessions}
 	comments := CommentHandler{store: store, sessions: sessions}
+	users := UserHandler{store: store, sessions: sessions}
 
 	h.Use(middleware.Logger)
 	h.Use(csrf.Protect(csrfKey, csrf.Secure(false)))
 	h.Use(sessions.LoadAndSave)
+	h.Use(h.withUser)
 
 	h.Get("/", h.Home())
 	h.Route("/threads", func(r chi.Router) {
@@ -45,6 +49,11 @@ func NewHandler(
 		r.Post("/{threadID}/{postID}", comments.Store())
 	})
 	h.Get("/comments/{id}/vote", comments.Vote())
+	h.Get("/register", users.Register())
+	h.Post("/register", users.RegisterSubmit())
+	h.Get("/login", users.Login())
+	h.Post("/login", users.LoginSubmit())
+	h.Get("/logout", users.Logout())
 
 	return h
 }
@@ -78,4 +87,19 @@ func (h *Handler) Home() http.HandlerFunc {
 			Posts:       pp,
 		})
 	}
+}
+
+func (h *Handler) withUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id, _ := h.sessions.Get(r.Context(), "user_id").(uuid.UUID)
+
+		user, err := h.store.User(id)
+		if err != nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
